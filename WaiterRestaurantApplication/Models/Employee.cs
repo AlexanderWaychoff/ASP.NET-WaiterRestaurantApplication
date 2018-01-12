db548df6515f83e282bf31b373ea40bd0d8add62
Alex Waychoff <AlexWaychoff@gmail.com>
2018-01-12 09:50:57 -0600

    Finished writing models

---
+++ WaiterRestaurantApplication/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WaiterRestaurantApplication.Models
+{
+    public class Employee
+    {
+    }
+}
