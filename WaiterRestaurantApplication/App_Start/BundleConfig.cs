faa50161ccf5097b2f5a50a0bc1240b031c447fc
Alex Waychoff <AlexWaychoff@gmail.com>
2018-01-12 10:26:33 -0600

    added sandstone css from bootswatch

--- WaiterRestaurantApplication/App_Start/BundleConfig.cs
+++ WaiterRestaurantApplication/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace WaiterRestaurantApplication
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-sandstone.min.css",
+                      //"~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
