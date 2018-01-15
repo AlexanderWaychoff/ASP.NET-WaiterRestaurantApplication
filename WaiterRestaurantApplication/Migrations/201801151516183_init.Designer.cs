f0da79c0c29f97996ef3f64a2e7bf00c4526407c
RickK213 <rkippert@att.net>
2018-01-15 09:17:06 -0600

    Fixed issue with Newtonsoft

--- WaiterRestaurantApplication/Migrations/201801151516183_init.Designer.cs
+++ WaiterRestaurantApplication/Migrations/201801151516183_init.Designer.cs
@@ -13,7 +13,7 @@ namespace WaiterRestaurantApplication.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201801151455196_init"; }
+            get { return "201801151516183_init"; }
         }
         
         string IMigrationMetadata.Source
